ae8070592948e92f47aeaff854f2c17f50ff7d17
Liliane Rodrigues <liliane.rodriguesssz@gmail.com>
2026-06-22 18:06:09 -0300

    Adiciona screenshots ao README

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyA3w_bPK1RIUd45bA9kEhGs9u19KwvHh1s",
+    authDomain: "imunikids-3ba21.firebaseapp.com",
+    projectId: "imunikids-3ba21",
+    storageBucket: "imunikids-3ba21.firebasestorage.app",
+    messagingSenderId: "310944493819",
+    appId: "1:310944493819:web:e0411e4fbcc58fc0eb005e"
+  }
 };
